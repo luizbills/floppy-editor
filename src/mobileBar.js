9ece24c956b3e70dc89ef6e3cc214c265bc3704d
Luiz Bills <luizbills@pm.me>
2023-12-29 11:04:14 -0300

    fix bar but remove search button

--- src/mobileBar.js
+++ src/mobileBar.js
@@ -3,6 +3,7 @@ import { openSearchPanel } from "@codemirror/search";
 
 export default function mobileBar(editorView) {
   let blurTimeout = 0;
+  let pendingUpdate = 0;
   const parent = editorView.dom.parentNode;
   const commands = [
     {
@@ -25,12 +26,12 @@ export default function mobileBar(editorView) {
       label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3" /></svg>`,
       callback: redo,
     },
-    {
-      name: "search (and replace)",
-      label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>`,
-      callback: openSearchPanel,
-      focus: false,
-    },
+    // {
+    //   name: "search (and replace)",
+    //   label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>`,
+    //   callback: openSearchPanel,
+    //   focus: false,
+    // },
   ];
 
   const buttons = document.createElement("div");
@@ -42,7 +43,7 @@ export default function mobileBar(editorView) {
     const focus = null == cmd.focus ? true : cmd.focus;
 
     button.innerHTML = cmd.label;
-    button.setAttribute("name", cmd.name);
+    button.setAttribute("title", cmd.name);
     button.setAttribute("aria-label", cmd.name);
     button.setAttribute("data-focus", focus ? "true" : "false");
 
@@ -53,36 +54,61 @@ export default function mobileBar(editorView) {
     });
 
     buttons.appendChild(button);
-
-    let pendingUpdate = false;
-    function viewportHandler(event) {
-      if (pendingUpdate) return;
-      const viewport = event.target;
-      pendingUpdate = true;
-      requestAnimationFrame(() => {
-        pendingUpdate = false;
-        buttons.style.top =
-          viewport.height - buttons.getBoundingClientRect().height + "px";
-      });
-    }
-
-    window.visualViewport.addEventListener("resize", viewportHandler);
   }
 
   parent.appendChild(buttons);
 
-  editorView.contentDOM.addEventListener("focus", (evt) => {
-    evt.preventDefault();
-    buttons.style.display = "";
-    if (blurTimeout) clearTimeout(blurTimeout);
+  requestAnimationFrame(updateButtonsBarPosition);
+
+  function viewportHandler(event) {
+    if (pendingUpdate) cancelAnimationFrame(pendingUpdate);
+    pendingUpdate = requestAnimationFrame(updateButtonsBarPosition);
+  }
+
+  visualViewport.addEventListener("resize", viewportHandler);
+  visualViewport.addEventListener("scroll", viewportHandler);
+
+  function updateButtonsBarPosition() {
+    clearTimeout(blurTimeout);
+    pendingUpdate = false;
+    viewport = window.visualViewport;
+
+    const screen = getScreenSize();
+    const barSize = buttons.getBoundingClientRect();
+    const scale = 1 / viewport.scale;
+
+    x = viewport.offsetLeft;
+    y = viewport.height + viewport.offsetTop - barSize.height;
+
+    buttons.style.transform = `translate(${x}px, ${y}px) scale(${scale})`;
+
+    if (viewport.height < screen.height && scale >= 0.98) {
+      showMobileBar();
+    } else {
+      hideMobileBar();
+    }
+  }
+
+  editorView.contentDOM.addEventListener("click", () => {
+    showMobileBar();
   });
 
-  editorView.contentDOM.addEventListener("blur", (evt) => {
-    evt.preventDefault();
-    blurTimeout = setTimeout(hideMobileBar, 100);
+  editorView.contentDOM.addEventListener("focus", () => {
+    showMobileBar();
   });
+
+  function getScreenSize() {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    };
+  }
 
   function hideMobileBar() {
     buttons.style.display = "none";
+  }
+
+  function showMobileBar() {
+    buttons.style.display = "";
   }
 }
